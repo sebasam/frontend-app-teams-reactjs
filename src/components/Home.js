77505dffd63df12e6ef9e4906de2ddd561bcf1ae
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-04 13:20:21 -0500

    events added to Home component from fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './../assets/styles/home.css'
+import { url } from './Const'
 
 export const Home = () => {
+    const [sport, setSports] = useState([])
     const requestOptions = {
         method: 'GET',
         headers: {
             "Content-Type": "application/json"
         }
     }
-    fetch('http://localhost:3001/api/users/', requestOptions)
+    fetch(`${ url }/api/events`, requestOptions)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            setSports(data.events)          
+        })
         .catch(err => console.log(err))
-
     return (
         <div id='home'>
-
+            <div className='container'>
+                <div id='events'>
+                    <h2 className='text-white'>Eventos</h2>
+                    { 
+                        sport.map(data => {
+                            return  <span className='text-white'>{ data.name  }</span>                       
+                        })
+                    }
+                </div>
+                <div className='text-white' id='teams'>
+                    Equipos
+                </div>
+            </div>
         </div>
     )
 }
