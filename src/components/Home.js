7125271d911eb358c1d4ee47200f125083151bbb
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-04 07:09:31 -0500

    API REST method get works now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    fetch('http://localhost:3001/api/users/', requestOptions)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+
+    return (
+        <div id='home'>
+
+        </div>
+    )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,22 +15,22 @@ const Navbar = () => {
         color: "#7300e6"       
     }
     return(
-            <nav className="navbar navbar-expand-lg bg-transparent">
-            <div className="container-fluid">
-                <a className="navbar-brand text-white" href="#">Quiimba Sports</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <NavLink to='/' end className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' } aria-current="page">Home</NavLink>
-                        <NavLink to='/categories' className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' } >Categorías</NavLink>
-                        <NavLink to='/teams' className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' } >Equipos</NavLink>
-                        <NavLink to='/events' className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' }>Eventos</NavLink>
-                    </div>
+        <nav className="navbar navbar-expand-lg bg-transparent">
+        <div className="container-fluid">
+            <a className="navbar-brand text-white" href="#">Quiimba Sports</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <NavLink to='/' end className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' } aria-current="page">Home</NavLink>
+                    <NavLink to='/categories' className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' } >Categorías</NavLink>
+                    <NavLink to='/teams' className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' } >Equipos</NavLink>
+                    <NavLink to='/events' className={ ({ isActive }) => isActive ? 'myActive nav-link text-white' : 'nav-link text-white' }>Eventos</NavLink>
                 </div>
-                </div>
-            </nav>
+            </div>
+            </div>
+        </nav>
         
     )
 }
