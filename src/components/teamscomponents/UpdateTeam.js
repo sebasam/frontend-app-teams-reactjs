f23690f612daece069b5764068669a0fc0f7ee3f
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-12 12:01:30 -0500

    updating teams

--- src/components/teamscomponents/UpdateTeam.js
+++ src/components/teamscomponents/UpdateTeam.js
@@ -27,11 +27,15 @@ export const UpdateTeam = (props) => {
             })
     }
 
+    useEffect(() => {
+        setId(props.id)
+    }, [props.id])
+
     return(
         <div id='container__update-team'>
             <input value={ myId } placeholder='Nombre de equipo a eliminar' className='form-control' />
-            <input value={ myId } placeholder='Nombre de equipo a eliminar' className='form-control' />
-            <button onClick={ deleteMyTeam } className='btn btn-primary'>Eliminar Equipo</button>
+            <input ref={ props.current } placeholder='Nombre de equipo a eliminar' className='form-control' />
+            <button onClick={ updateMyTeam } className='btn btn-primary'>Actualizar Equipo</button>
         </div>
     )
 }
