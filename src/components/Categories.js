0e9593dff10a1be10108cb4bd1e0f040a395bae7
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-12 19:52:17 -0500

    teams can be updated, category restfull in process

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,14 +1,62 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './../assets/styles/categories.css'
+import { url } from './Const'
+import Swal from 'sweetalert2'
+
 
 export const Categories = () => {
+    const refName = useRef(null)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: refName.current.value })         
+        }
+        fetch(`${ url }/api/categories`, requestOptions)
+            .then(res => { res.json() })
+            .then(data => { 
+                if(data.ok) {
+                    Swal.fire(
+                        'Excellent!',
+                        `${ data.msg }`,
+                        'success'
+                    )
+                } else {
+                    Swal.fire(
+                        'Ups!!',
+                        `${ data.msg }`,
+                        'error'
+                    )
+                }
+                })
+                .catch(error => {
+                    Swal.fire(
+                        'Ups!!',
+                        'Check the fields',
+                        'error'
+                    )
+                })
+    }
 
     return (
         <div id='categories__container'>
             <form>
-                <input placeholder='Nombre de categoría' />
-                <button>Crear Categoría</button>
+                <input ref={ refName } className='form-control' placeholder='Nombre de categoría' />
+                <button onClick={ handleSubmit } type='submit' className='btn btn-danger'>Crear Categoría</button>
             </form>
-        </div>
+            <div id='categories__generate'>
+
+            </div>
+            <div id='delete__container--categories'>
+            
+            </div>
+            <div id='update__container--categories'>
+            
+            </div>
+        </div>        
     )
 }
